29e8723601a2d6681e197f84a0eba51e1f5e4ed3
Salil999 <ssaxen4@illinois.edu>
2018-03-28 23:24:26 -0500

    consumer now ingests and chunks log files into appropriate sizes

--- src/kafka/LittleLogConsumer.java
+++ src/kafka/LittleLogConsumer.java
@@ -16,18 +16,21 @@ public class LittleLogConsumer<K, V> implements Runnable {
 	private final KafkaConsumer<K, V> consumer;
 	private final Class keyClass;
 	private final Class valueClass;
+	private final long chunkSize;
 
 	public LittleLogConsumer() {
-		this("test", "localhost:9092", StringDeserializer.class, StringDeserializer.class);
+		this("test", "localhost:9092", 1e9, StringDeserializer.class, StringDeserializer.class);
 	}
 
-	public LittleLogConsumer(final Properties props, final Class keyClass, final Class valueClass) {
+	public LittleLogConsumer(final Properties props, final Class keyClass, final long chunkSize, final Class valueClass) {
 		this.consumer = new KafkaConsumer<>(props);
 		this.keyClass = keyClass;
 		this.valueClass = valueClass;
+		this.chunkSize = chunkSize;
 	}
 
-	public LittleLogConsumer(final String topic, final String bootstrapServers, final Class keyClass, final Class valueClass) {
+	public LittleLogConsumer(final String topic, final String bootstrapServers, final long chunkSize, final Class
+			keyClass, final Class valueClass) {
 		final Properties props = new Properties();
 		props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
 		props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, keyClass.getName());
@@ -37,19 +40,21 @@ public class LittleLogConsumer<K, V> implements Runnable {
 		this.valueClass = valueClass;
 		this.consumer = new KafkaConsumer<>(props);
 		this.consumer.subscribe(Collections.singleton(topic));
+		this.chunkSize = chunkSize;
 	}
 
 	@Override
 	public void run() {
+		System.out.println("Consumer started!");
 		try {
 			FileWriter writer;
 			File directory;
-			long currentSize = 0;
-			final int maxLogNumber = 0;
+			long currentSize;
+			int maxLogNumber = 0;
 			while (true) {
 				final ConsumerRecords<K, V> consumerRecords = this.consumer.poll(500);
 
-				directory = new File("/Users/Salil/Desktop/LittleLog/logs/ll-" + maxLogNumber + ".log");
+				directory = new File("/Users/Salil/Desktop/LittleLog/logs/ll_" + maxLogNumber + ".log");
 				if (!directory.exists()) {
 					directory.getParentFile().mkdirs();
 					directory.createNewFile();
@@ -61,16 +66,14 @@ public class LittleLogConsumer<K, V> implements Runnable {
 					final String value = record.value().toString();
 					writer.write(String.format("%s, %s\n", key, value));
 				}
+
 				writer.close();
 				currentSize = directory.length();
-				if (currentSize > 1e7) {
-//				maxLogNumber++;
-					break;
+				if (currentSize > this.chunkSize) {
+					maxLogNumber++;
 				}
 				this.consumer.commitAsync();
 			}
-			this.consumer.close();
-			System.out.println("Finished!");
 		} catch (final Exception e) {
 			e.printStackTrace();
 		}
