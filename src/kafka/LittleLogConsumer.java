8031903bc2f4daeb2d4a65e0a9bdfa3186fd97c2
Salil999 <ssaxen4@illinois.edu>
2018-04-01 15:36:00 -0500

    forgot to add important class

--- src/kafka/LittleLogConsumer.java
+++ src/kafka/LittleLogConsumer.java
@@ -1,25 +1,28 @@
 package kafka;
 
+import littlelog.LittleLog;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.util.Collections;
 import java.util.Properties;
 
 public class LittleLogConsumer<K, V> implements Runnable {
 
+	public static final double ONE_MB = 1e6;
 	private final KafkaConsumer<K, V> consumer;
 	private final Class keyClass;
 	private final Class valueClass;
 	private final double chunkSize;
 
 	public LittleLogConsumer() {
-		this("test", "localhost:9092", 1e9, StringDeserializer.class, StringDeserializer.class);
+		this("test", "localhost:9092", 20.0 * ONE_MB, StringDeserializer.class, StringDeserializer
+				.class);
 	}
 
 	public LittleLogConsumer(final Properties props, final Class keyClass, final double chunkSize, final Class
@@ -45,12 +48,6 @@ public class LittleLogConsumer<K, V> implements Runnable {
 		this.chunkSize = chunkSize;
 	}
 
-	public LittleLogConsumer(final Properties props, final Class keyClass, final Class valueClass) {
-		this.consumer = new KafkaConsumer<>(props);
-		this.keyClass = keyClass;
-		this.valueClass = valueClass;
-	}
-
 	@Override
 	public void run() {
 		System.out.println("Consumer started!");
@@ -59,27 +56,36 @@ public class LittleLogConsumer<K, V> implements Runnable {
 			File directory;
 			long currentSize;
 			int maxLogNumber = 0;
-			while (true) {
-				final ConsumerRecords<K, V> consumerRecords = this.consumer.poll(500);
+			final LittleLog littleLog = new LittleLog("compressed_logs/");
+			final BufferedReader reader = new BufferedReader(new FileReader("../kafka_scripts/http.log"));
+			String currLine;
+			while ((currLine = reader.readLine()) != null) {
+//				final ConsumerRecords<K, V> consumerRecords = this.consumer.poll(500);
 
-				directory = new File("/Users/Salil/Desktop/LittleLog/logs/ll_" + maxLogNumber + ".log");
+				directory = new File("/Users/Salil/Desktop/LittleLog/src/logs/ll_" + maxLogNumber + ".log");
 				if (!directory.exists()) {
 					directory.getParentFile().mkdirs();
 					directory.createNewFile();
 				}
 
 				writer = new FileWriter(directory, true);
-				for (final ConsumerRecord<K, V> record : consumerRecords) {
-					final String key = record.key().toString();
-					final String value = record.value().toString();
-					writer.write(String.format("%s, %s\n", key, value));
-				}
+				writer.write(currLine + "\n");
+
+//				for (final ConsumerRecord<K, V> record : consumerRecords) {
+//					final String key = record.key().toString();
+//					final String value = record.value().toString();
+//				    writer.write(String.format("%s, %s\n", key, value));
+//				}
 
 				writer.close();
 				currentSize = directory.length();
+
 				if (currentSize > this.chunkSize) {
+					littleLog.compress("logs/ll_" + maxLogNumber + ".log");
 					maxLogNumber++;
+
 				}
+
 				this.consumer.commitAsync();
 			}
 		} catch (final Exception e) {
