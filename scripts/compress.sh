763b74ae7487b150e3deecd9aed475dacdfb746c
rsurti2 <rsurti2@illinois.edu>
2018-04-18 03:54:24 -0500

    numthreads cli param

--- scripts/compress.sh
+++ scripts/compress.sh
@@ -2,5 +2,5 @@
 
 cd $(pwd)/../src/
 javac -cp .:$(pwd)/../lib/* littlelog/*.java unit_tests/*.java
-time java -cp .:$(pwd)/../lib/* unit_tests/Compress $1 $2
+time java -Xmx128G  -cp .:$(pwd)/../lib/* unit_tests/Compress $1 $2 $3
 find . -type f -path "*/*" -name "*.class" -exec rm -f {} \;
