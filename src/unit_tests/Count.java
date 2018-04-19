fc172782c487046cfce81be1a85d4f037a65d9df
rsurti2 <rsurti2@illinois.edu>
2018-04-18 23:13:14 -0500

    100 threads default

--- src/unit_tests/Count.java
+++ src/unit_tests/Count.java
@@ -7,7 +7,7 @@ import java.io.File;
 public class Count {
     public static void main(final String[] args) {
         if (args.length == 2) {
-            final LittleLog littleLog = new LittleLog(10);
+            final LittleLog littleLog = new LittleLog(100);
             littleLog.count(args[0], new File(args[1]));
             littleLog.shutdown();
         } else {
--- src/unit_tests/Grep.java
+++ src/unit_tests/Grep.java
@@ -7,7 +7,7 @@ import java.io.File;
 public class Grep {
     public static void main(final String[] args) {
         if (args.length == 2) {
-            final LittleLog littleLog = new LittleLog(10);
+            final LittleLog littleLog = new LittleLog(100);
             littleLog.query(args[0], new File(args[1]));
             littleLog.shutdown();
         } else {
