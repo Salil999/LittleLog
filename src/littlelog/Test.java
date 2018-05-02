d46bb1af4f3273ffb4b4eb4e77c690694cee5d2c
rsurti2 <rsurti2@illinois.edu>
2018-05-02 05:00:48 -0500

    extract binary search

--- src/littlelog/Test.java
+++ src/littlelog/Test.java
@@ -1,12 +1,10 @@
 package littlelog;
 
-import java.io.File;
-
 public class Test {
     public static void main(final String[] args) {
-        final File file = new File("logfiles/access_compressed/access_01.succinct");
+//        final File file = new File("logfiles/access_compressed/access_01.succinct");
 
-        final SuccinctLog succinctLog = new SuccinctLog(file.getAbsolutePath());
+//        final SuccinctLog succinctLog = new SuccinctLog(file.getAbsolutePath());
 //        succinctLog.findNewlines();
 //        final LittleLog littleLog = new LittleLog();
 //        littleLog.query("\n", file);
