38cc08708ec35a80094aefe85d580487354755e7
rsurti2 <rsurti2@illinois.edu>
2018-05-03 13:01:03 -0500

    compress all scripts

--- scripts/compress_all.sh
+++ scripts/compress_all.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
-nohup ./littlelog -c -i=../../logfiles/input_logs/http.log -o=../../logfiles/succinct_logs/out_200 -s=200 -t=10
- > nohup_200.out &
-
-#nohup ./littlelog -c -i=../../logfiles/input_logs/http.log -o=../../logfiles/succinct_logs/out_500 -s=500 -t=10
-# > nohup_500.out &
+nohup ./littlelog -c -i=../../logfiles/input_logs/http.log -o=../../logfiles/succinct_logs/out_200 -s=200 -t=10 > nohup_200.out &
