e6ff86860d28c79dbb7da1a7d6906d75c7ca2ef4
rsurti2 <rsurti2@illinois.edu>
2018-05-03 22:29:47 -0500

    grep script

--- scripts/compress_test.sh
+++ scripts/compress_test.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
-#nohup ./littlelog -c -i=../../logfiles/input_logs/http.log -o=../../logfiles/succinct_logs/out_200 -s=200 -t=10 > nohup_200.out &
-
-#./littlelog -c -i=../../logfiles/input_logs/http.log -o=../../logfiles/succinct_logs/out_200 -s=200 -t=5
-
 nohup ./littlelog -c -i=../../logfiles/input_logs/http.log -o=../../logfiles/succinct_logs/out_$1 -s=$1 -t=$2 > nohup_$1.out 2>&1 &
---
+++ scripts/grep_succinct.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+nohup ./littlelog -i=../../logfiles/succinct_logs/out_$1 -g "$2" -t=100 > nohup_grep_$1_$2.out 2>&1 &
