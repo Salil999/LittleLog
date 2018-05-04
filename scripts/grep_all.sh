b40fd1b689fe5418fdaec1bb7fc09289137cf51d
rsurti2 <rsurti2@illinois.edu>
2018-05-04 04:35:25 -0500

    grep test script

--- scripts/grep_all.sh
+++ scripts/grep_all.sh
@@ -2,23 +2,41 @@
 
 
 function grep_succinct () {
+    local shard_size=$1
+    local regex=$2
+
+    for run in {1..5}
+    do
+#        echo $1 $2
+        ./littlelog -i=../../logfiles/succinct_logs/out_"$1" -g "$2" -t=100 | wc -l
+    done
+}
+
+function grep_all () {
     local regex=$1
     mkdir grep_"$regex"
-    ./grep_test_helper.sh 20 regex 1> grep_"$regex"/20mb_results.txt 2>&1
-    ./grep_test_helper.sh 50 regex 1> grep_"$regex"/50mb_results.txt 2>&1
-    ./grep_test_helper.sh 100 regex 1> grep_"$regex"/100mb_results.txt 2>&1
-    ./grep_test_helper.sh 200 regex 1> grep_"$regex"/200mb_results.txt 2>&1
-    ./grep_test_helper.sh 300 regex 1> grep_"$regex"/300mb_results.txt 2>&1
-    ./grep_test_helper.sh 500 regex 1> grep_"$regex"/500mb_results.txt 2>&1
+
+    declare -a shard_sizes=("20"
+                            "50"
+                            "100"
+                            "200"
+                            "300"
+                            "500"
+                            )
+
+    for i in "${shard_sizes[@]}"
+    do
+       grep_succinct "$i" "$regex"
+    done
+
     time grep "$regex "../../logfiles/input_logs/http.log > grep_"$regex"/grep_"$regex"_results.txt
 }
 
 
-declare -a arr=("228.34.77.223"
-                "Dec/2015"
-                )
-
-for i in "${arr[@]}"
+declare -a regex_queries=(  "228.34.77.223"
+                            "Dec"
+                            )
+for r in "${regex_queries[@]}"
 do
-   grep_succinct "$i"
+   grep_all "$r"
 done
