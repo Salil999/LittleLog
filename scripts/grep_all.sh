d61ab9b4bde34a72c690f9ae3961bca092d858ae
rsurti2 <rsurti2@illinois.edu>
2018-05-04 04:07:25 -0500

    grep test script

--- scripts/grep_all.sh
+++ scripts/grep_all.sh
@@ -3,15 +3,15 @@
 
 function grep_succinct () {
     local regex=$1
-    echo "$regex"
-#    mkdir grep_regex
-#    ./grep_test_helper.sh 20 regex 1> grep_regex/20mb_results.txt 2>&1
-#    ./grep_test_helper.sh 50 regex 1> grep_regex/20mb_results.txt 2>&1
-#    ./grep_test_helper.sh 100 regex 1> grep_regex/20mb_results.txt 2>&1
-#    ./grep_test_helper.sh 200 regex 1> grep_regex/20mb_results.txt 2>&1
-#    ./grep_test_helper.sh 300 regex 1> grep_regex/20mb_results.txt 2>&1
-#    ./grep_test_helper.sh 500 regex 1> grep_regex/20mb_results.txt 2>&1
-#    time grep regex ../../logfiles/input_logs/http.log > grep_regex/grep_regex_results.txt
+#    echo "$regex"
+    mkdir grep_regex
+    ./grep_test_helper.sh 20 regex 1> grep_regex/20mb_results.txt 2>&1
+    ./grep_test_helper.sh 50 regex 1> grep_regex/20mb_results.txt 2>&1
+    ./grep_test_helper.sh 100 regex 1> grep_regex/20mb_results.txt 2>&1
+    ./grep_test_helper.sh 200 regex 1> grep_regex/20mb_results.txt 2>&1
+    ./grep_test_helper.sh 300 regex 1> grep_regex/20mb_results.txt 2>&1
+    ./grep_test_helper.sh 500 regex 1> grep_regex/20mb_results.txt 2>&1
+    time grep regex ../../logfiles/input_logs/http.log > grep_regex/grep_regex_results.txt
 }
 
 
