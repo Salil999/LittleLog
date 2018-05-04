b8f7612234ae135a6a236b58f2a36cca367069fe
rsurti2 <rsurti2@illinois.edu>
2018-05-04 00:02:11 -0500

    grep script

--- scripts/grep_test.sh
+++ scripts/grep_test.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-nohup ./littlelog -i=../../logfiles/succinct_logs/out_$1 -g "$2" -t=100 > nohup_grep_$1_$2.out 2>&1 &
+echo nohup ./littlelog -i=../../logfiles/succinct_logs/out_$1 -g "$2" -t=100 > nohup_grep_$1_$2.out 2>&1 &
 
-nohup time grep "$2" ../../logfiles/input_logs/http.log > nohup_grep_$2.out 2>&1 &
+echo nohup time grep "$2" ../../logfiles/input_logs/http.log > nohup_grep_$2.out 2>&1 &
