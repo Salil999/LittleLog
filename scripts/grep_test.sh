751c0112b8462af00e3b5b778dbdc61a97ce25ca
rsurti2 <rsurti2@illinois.edu>
2018-05-04 03:02:58 -0500

    grep test script

--- scripts/grep_test.sh
+++ scripts/grep_test.sh
@@ -1,9 +1,3 @@
 #!/usr/bin/env bash
 
-./littlelog -i=../../logfiles/succinct_logs/out_$1 -g "$2" -t=100 | wc -l
-./littlelog -i=../../logfiles/succinct_logs/out_$1 -g "$2" -t=100 | wc -l
-./littlelog -i=../../logfiles/succinct_logs/out_$1 -g "$2" -t=100 | wc -l
-./littlelog -i=../../logfiles/succinct_logs/out_$1 -g "$2" -t=100 | wc -l
-./littlelog -i=../../logfiles/succinct_logs/out_$1 -g "$2" -t=100 | wc -l
-
-#./littlelog -i=../../logfiles/succinct_logs/out_$1 -g "$2" -t=100 | wc -l >> grep_$1_$2.out 2>&1
+./grep_test_helper.sh $1 $2 >> grep_$1mb_$2
