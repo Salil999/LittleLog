6adae1fe1ad5e4e05a5c2cb80b4b1cd490c9abd3
rsurti2 <rsurti2@illinois.edu>
2018-05-04 02:51:58 -0500

    grep test script

--- scripts/grep_test.sh
+++ scripts/grep_test.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-./littlelog -i=../../logfiles/succinct_logs/out_$1 -g "$2" -t=100 | wc -l >> grep_$1_$2.out 2>&1
-./littlelog -i=../../logfiles/succinct_logs/out_$1 -g "$2" -t=100 | wc -l >> grep_$1_$2.out 2>&1
-./littlelog -i=../../logfiles/succinct_logs/out_$1 -g "$2" -t=100 | wc -l >> grep_$1_$2.out 2>&1
-./littlelog -i=../../logfiles/succinct_logs/out_$1 -g "$2" -t=100 | wc -l >> grep_$1_$2.out 2>&1
-./littlelog -i=../../logfiles/succinct_logs/out_$1 -g "$2" -t=100 | wc -l >> grep_$1_$2.out 2>&1
+./littlelog -i=../../logfiles/succinct_logs/out_$1 -g "$2" -t=100 | wc -l
+./littlelog -i=../../logfiles/succinct_logs/out_$1 -g "$2" -t=100 | wc -l
+./littlelog -i=../../logfiles/succinct_logs/out_$1 -g "$2" -t=100 | wc -l
+./littlelog -i=../../logfiles/succinct_logs/out_$1 -g "$2" -t=100 | wc -l
+./littlelog -i=../../logfiles/succinct_logs/out_$1 -g "$2" -t=100 | wc -l
+
+#./littlelog -i=../../logfiles/succinct_logs/out_$1 -g "$2" -t=100 | wc -l >> grep_$1_$2.out 2>&1
