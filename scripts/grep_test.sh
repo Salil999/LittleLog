18643a379c51a2e8d3e2f7ad320649efc7116fc0
rsurti2 <rsurti2@illinois.edu>
2018-05-04 01:51:57 -0500

    grep test script

--- scripts/grep_test.sh
+++ scripts/grep_test.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-./littlelog -i=../../logfiles/succinct_logs/out_$1 -g "$2" -t=100 >> nohup_grep_$1_$2.out
-./littlelog -i=../../logfiles/succinct_logs/out_$1 -g "$2" -t=100 >> nohup_grep_$1_$2.out
-./littlelog -i=../../logfiles/succinct_logs/out_$1 -g "$2" -t=100 >> nohup_grep_$1_$2.out
+./littlelog -i=../../logfiles/succinct_logs/out_$1 -g "$2" -t=100 >> nohup_grep_$1_$2.out 2>&1
+./littlelog -i=../../logfiles/succinct_logs/out_$1 -g "$2" -t=100 >> nohup_grep_$1_$2.out 2>&1
+./littlelog -i=../../logfiles/succinct_logs/out_$1 -g "$2" -t=100 >> nohup_grep_$1_$2.out 2>&1
 
 #nohup ./littlelog -i=../../logfiles/succinct_logs/out_$1 -g "$2" -t=100 > nohup_grep_$1_$2.out 2>&1 &
 #
