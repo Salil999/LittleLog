37ec27d1a6e0326fa9a793b3d6bc7c54f7e233a5
rsurti2 <rsurti2@illinois.edu>
2018-05-04 21:03:43 -0500

    grep test script

--- scripts/grep_all.sh
+++ scripts/grep_all.sh
@@ -43,12 +43,13 @@
 function grep_succinct () {
     local regex=$1
     mkdir grep_"$regex"
+    ./grep_test.sh 500 "$regex"
 #    ./grep_test_helper.sh 20 regex 1> grep_"$regex"/20mb_results.txt 2>&1
 #    ./grep_test_helper.sh 50 regex 1> grep_"$regex"/50mb_results.txt 2>&1
 #    ./grep_test_helper.sh 100 regex 1> grep_"$regex"/100mb_results.txt 2>&1
 #    ./grep_test_helper.sh 200 regex 1> grep_"$regex"/200mb_results.txt 2>&1
 #    ./grep_test_helper.sh 300 regex 1> grep_"$regex"/300mb_results.txt 2>&1
-    ./grep_test_helper.sh 500 regex 1> grep_"$regex"/500mb_results.txt 2>&1
+#    ./grep_test_helper.sh 500 regex 1> grep_"$regex"/500mb_results.txt 2>&1
 #    time grep "$regex "../../logfiles/input_logs/http.log > grep_"$regex"/grep_"$regex"_results.txt
 }
 
