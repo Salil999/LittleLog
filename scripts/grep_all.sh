17e2e3253dfec862af95a878d6b26df646e75f57
rsurti2 <rsurti2@illinois.edu>
2018-05-05 18:08:01 -0500

    count all script

--- scripts/grep_all.sh
+++ scripts/grep_all.sh
@@ -3,19 +3,19 @@
 function grep_succinct () {
     local regex=$1
     mkdir grep_"$regex"
-    ./grep_succinct.sh 20 "$regex"
-    ./grep_succinct.sh 50 "$regex"
-    ./grep_succinct.sh 100 "$regex"
-    ./grep_succinct.sh 200 "$regex"
-    ./grep_succinct.sh 300 "$regex"
-    ./grep_succinct.sh 500 "$regex"
+#    ./grep_succinct.sh 20 "$regex"
+#    ./grep_succinct.sh 50 "$regex"
+#    ./grep_succinct.sh 100 "$regex"
+#    ./grep_succinct.sh 200 "$regex"
+#    ./grep_succinct.sh 300 "$regex"
+#    ./grep_succinct.sh 500 "$regex"
     ./grep_file.sh "$regex"
 }
 
 declare -a arr=(
-#                "228.34.77.223"
-#                "02:32:17"
-#                "2023"
+                "228.34.77.223"
+                "02:32:17"
+                "2023"
                 "PUT"
                 "GET"
                 "HTTP"
