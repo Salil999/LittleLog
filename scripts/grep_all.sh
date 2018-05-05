e7cc02a693ae286adda0329b15779c37ac5dbbc6
rsurti2 <rsurti2@illinois.edu>
2018-05-04 21:44:12 -0500

    grep test script

--- scripts/grep_all.sh
+++ scripts/grep_all.sh
@@ -3,13 +3,13 @@
 function grep_succinct () {
     local regex=$1
     mkdir grep_"$regex"
-    grep_succinct.sh 20 "$regex"
-    grep_succinct.sh 50 "$regex"
-    grep_succinct.sh 100 "$regex"
-    grep_succinct.sh 200 "$regex"
-    grep_succinct.sh 300 "$regex"
-    grep_succinct.sh 500 "$regex"
-    grep_file.sh "$regex"
+    ./grep_succinct.sh 20 "$regex"
+    ./grep_succinct.sh 50 "$regex"
+    ./grep_succinct.sh 100 "$regex"
+    ./grep_succinct.sh 200 "$regex"
+    ./grep_succinct.sh 300 "$regex"
+    ./grep_succinct.sh 500 "$regex"
+    ./grep_file.sh "$regex"
 }
 
 declare -a arr=("228.34.77.223"
