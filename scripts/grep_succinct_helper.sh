5b5e8b99f6b1d66118369ef43de985c386ab78e6
rsurti2 <rsurti2@illinois.edu>
2018-05-04 21:58:26 -0500

    grep test script

--- scripts/grep_succinct_helper.sh
+++ scripts/grep_succinct_helper.sh
@@ -3,8 +3,8 @@
 
 #echo "mb $1"
 #echo "regex $2"
-./littlelog -i=../../logfiles/succinct_logs/out_"$1" -g "$2" -t=100
-#./littlelog -i=../../logfiles/succinct_logs/out_"$1" -g "$2" -t=100 | wc -l
-#./littlelog -i=../../logfiles/succinct_logs/out_"$1" -g "$2" -t=100 | wc -l
-#./littlelog -i=../../logfiles/succinct_logs/out_"$1" -g "$2" -t=100 | wc -l
-#./littlelog -i=../../logfiles/succinct_logs/out_"$1" -g "$2" -t=100 | wc -l
+./littlelog -i=../../logfiles/succinct_logs/out_"$1" -g "$2" -t=100 | wc -l
+./littlelog -i=../../logfiles/succinct_logs/out_"$1" -g "$2" -t=100 | wc -l
+./littlelog -i=../../logfiles/succinct_logs/out_"$1" -g "$2" -t=100 | wc -l
+./littlelog -i=../../logfiles/succinct_logs/out_"$1" -g "$2" -t=100 | wc -l
+./littlelog -i=../../logfiles/succinct_logs/out_"$1" -g "$2" -t=100 | wc -l
