4fb7e1723367530fb7a30eca44114ce997cc784f
rsurti2 <rsurti2@illinois.edu>
2018-05-06 03:09:12 -0500

    full test script

--- scripts/count_file.sh
+++ scripts/count_file.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-./count_file_helper.sh $1 1> count_$1/count_$1_results.txt 2>&1
+./count_file_helper.sh $1 1> results/count/count_$1/count_$1_results.txt 2>&1
--- scripts/grep_file.sh
+++ scripts/grep_file.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-./grep_file_helper.sh $1 1> grep_$1/grep_$1_results.txt 2>&1
+./grep_file_helper.sh $1 1> resulsts/grep/grep_$1/grep_$1_results.txt 2>&1
