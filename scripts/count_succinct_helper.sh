2598c512f342d6dbb3c46ed282f171671bc07439
rsurti2 <rsurti2@illinois.edu>
2018-05-06 03:12:00 -0500

    full test script

--- scripts/count_succinct_helper.sh
+++ scripts/count_succinct_helper.sh
@@ -3,5 +3,5 @@
 #echo "mb $1"
 #echo "regex $2"
 ./littlelog -i=../../logfiles/succinct_logs/out_"$1" -n "$2" -t=100 -l=10
-#./littlelog -i=../../logfiles/succinct_logs/out_"$1" -n "$2" -t=100 -l=10
-#./littlelog -i=../../logfiles/succinct_logs/out_"$1" -n "$2" -t=100 -l=10
+./littlelog -i=../../logfiles/succinct_logs/out_"$1" -n "$2" -t=100 -l=10
+./littlelog -i=../../logfiles/succinct_logs/out_"$1" -n "$2" -t=100 -l=10
