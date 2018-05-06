ad5d37be5261c91ac447591dd746832209548abe
rsurti2 <rsurti2@illinois.edu>
2018-05-06 05:50:15 -0500

    full test script

--- scripts/grep_all.sh
+++ scripts/grep_all.sh
@@ -5,18 +5,18 @@
 function grep_succinct () {
     local regex=$1
     mkdir results/grep/grep_"$regex"
-    ./grep_succinct.sh 20 "$regex"
-    ./grep_succinct.sh 50 "$regex"
-    ./grep_succinct.sh 100 "$regex"
-    ./grep_succinct.sh 200 "$regex"
-    ./grep_succinct.sh 300 "$regex"
-    ./grep_succinct.sh 500 "$regex"
-    ./grep_succinct.sh 1000 "$regex"
+#    ./grep_succinct.sh 20 "$regex"
+#    ./grep_succinct.sh 50 "$regex"
+#    ./grep_succinct.sh 100 "$regex"
+#    ./grep_succinct.sh 200 "$regex"
+#    ./grep_succinct.sh 300 "$regex"
+#    ./grep_succinct.sh 500 "$regex"
+#    ./grep_succinct.sh 1000 "$regex"
     ./grep_file.sh "$regex"
 }
 
 declare -a arr=(
-#                "228.34.77.223"
+                "228.34.77.223"
                 "02:32:17"
                 "2023"
                 "PUT"
